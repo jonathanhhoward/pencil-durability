49b3eff8adabfac972cb97ada99555ac7631f49a
jonathanhhoward <2773262@gmail.com>
2020-06-02 10:35:54 -0400

    update the description

--- src/main/headers/Pencil.h
+++ src/main/headers/Pencil.h
@@ -2,8 +2,9 @@
  * (C) Copyright 2020, by Jonathan Howard
  *
  * This is my original solution to the Pencil Durability Kata for Pillar Technology
- * which describes a pencil with a point and an eraser that degrades when it writes
- * new text, erases text, and fills in erased text.
+ * which describes a Pencil with a Point and an Eraser that degrades when it writes
+ * text, erases text, and fills in erased text. A Pencil works with any medium that
+ * holds a string, and it makes changes directly to that string.
  *
  * https://github.com/PillarTechnology/kata-pencil-durability
  */
